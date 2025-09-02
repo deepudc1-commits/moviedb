981fd040fbc87569a38a9fa6b90fc18bc2687f6a
Deepankar Chauhan (ALLYIS INC) <v-chaud@microsoft.com>
2025-09-02 14:21:50 +0530

    Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,26 @@ import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm px-10">
-        <div className="flex-1">
-            <Link className="btn btn-ghost text-xl" to='/'>FilmsVault</Link>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><NavLink to='/movies'>Movies</NavLink></li>
+              <li><NavLink to='/shows'>TV Shows</NavLink></li>
+              <li><NavLink to='/people'>People</NavLink></li>
+            </ul>
+          </div>
+          <Link className="btn btn-ghost text-xl" to='/'>FilmsVault</Link>
         </div>
-        <div className="flex-none">
+        <div className="navbar-end hidden md:flex">
             <ul className="menu menu-horizontal px-1">
-            <li><NavLink to='/movies'>Movies</NavLink></li>
-            <li><NavLink to='/shows'>TV Shows</NavLink></li>
-            <li><NavLink to='/people'>People</NavLink></li>
+              <li><NavLink to='/movies'>Movies</NavLink></li>
+              <li><NavLink to='/shows'>TV Shows</NavLink></li>
+              <li><NavLink to='/people'>People</NavLink></li>
             </ul>
         </div>
     </div>
