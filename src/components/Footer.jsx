cfe0fdf57e007dbedd32417fa73e5703b82b9aa0
Deepankar Chauhan (ALLYIS INC) <v-chaud@microsoft.com>
2025-09-17 15:47:49 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
         <aside>
-            <p>Copyright © {new Date().getFullYear()} - All right reserved by FilmsVault</p>
+            <p className='mar-bot0'>Copyright © {new Date().getFullYear()} - All right reserved by FilmsVault</p>
         </aside>
     </footer>
   )
